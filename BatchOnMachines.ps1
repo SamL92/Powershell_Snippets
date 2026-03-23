4c7ac72888258cd1738d89d3a31bfdb8592fd402
Sam Large <35735360+SamL92@users.noreply.github.com>
2026-03-23 08:23:16 +0000

    Fix credential retrieval method in BatchOnMachines.ps1

--- BatchOnMachines.ps1
+++ BatchOnMachines.ps1
@@ -1,6 +1,6 @@
 $machines = "Array","of","names"
 
 #This seems to work fine on non-domain machines and no \machineName\User is needed, just user/pw.
-$creds = Get-Credentials
+$creds = Get-Credential
 
 Invoke-Command -Session $machines -FilePath "M:\software\myPowershellScript.ps1"
