8de77c66f47d16d4a97351dd352da8888d5eb855
Sam Large <35735360+SamL92@users.noreply.github.com>
2026-03-23 08:19:26 +0000

    Update BatchOnMachines.ps1 with credential notes
    Added comments for clarity on credential usage.

--- BatchOnMachines.ps1
+++ BatchOnMachines.ps1
@@ -1,4 +1,6 @@
 $machines = "Array","of","names"
+
+#This seems to work fine on non-domain machines and no \machineName\User is needed, just user/pw.
 $creds = Get-Credentials
 
 Invoke-Command -Session $machines -FilePath "M:\software\myPowershellScript.ps1"
